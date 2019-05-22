3c1e86f51265954e4b1a686d715939ed021aa4df
Adam Pietrasiak <adam@pietrasiak.com>
2019-05-22 13:38:52 +0200

    disabled cors

--- src/services/apollo/index.ts
+++ src/services/apollo/index.ts
@@ -3,7 +3,10 @@ import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { defaultDataIdFromObject, InMemoryCache } from 'apollo-cache-inmemory';
 
-const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URL });
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_API_URL,
+  fetchOptions: { mode: 'no-cors' },
+});
 
 const AppLink = ApolloLink.from([httpLink]);
 
