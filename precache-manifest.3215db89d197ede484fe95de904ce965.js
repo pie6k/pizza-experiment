74316092d1711e38b8d96a533b77198662abf0f7
Adam Pietrasiak <adam@pietrasiak.com>
2019-05-22 13:35:10 +0200

    Updates

---
+++ precache-manifest.3215db89d197ede484fe95de904ce965.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccad503c0a994fa315e63164e2390da9",
+    "url": "/pizza-experiment/index.html"
+  },
+  {
+    "revision": "cc4b8be60fd4a8ef33b1",
+    "url": "/pizza-experiment/static/js/2.e2cbd27c.chunk.js"
+  },
+  {
+    "revision": "0f4606e17c58a9b0a229",
+    "url": "/pizza-experiment/static/js/main.59cdf35d.chunk.js"
+  },
+  {
+    "revision": "f5a4462344825366eb0e",
+    "url": "/pizza-experiment/static/js/runtime~main.95ef854d.js"
+  }
+]);
